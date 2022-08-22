6ab38723ba1a3631153297d7def131a4e251f729
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-22 13:18:40 +0200

    Creating a list of people

--- src/main/java/com/irondif/ControladorInicio.java
+++ src/main/java/com/irondif/ControladorInicio.java
@@ -1,6 +1,10 @@
 package com.irondif;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,16 +22,31 @@ public class ControladorInicio {
 	@GetMapping("/")
 	public String inicio(Model model) {
 		String mensaje = "Mensaje con Thymeleaf";
+		
 		Persona persona = new Persona();
 		persona.setNombre("Daniel");
 		persona.setApellido("Espanadero");
 		persona.setEmail("d.espanadero@gmail.com");
 		persona.setTelefono("633282171");
 		
+		Persona persona2 = new Persona();
+		persona2.setNombre("Karla");
+		persona2.setApellido("Perez");
+		persona2.setEmail("karla@gmail.com");
+		persona2.setTelefono("633282183");
+		
+//		ArrayList<Persona> personas = new ArrayList<Persona>();
+//		personas.add(persona);
+//		personas.add(persona2);
+
+		List<Persona> personas = Arrays.asList(persona, persona2);
+//		List<Persona> personas = Arrays.asList();
+		
 //		log.info("ejecutando el controlador rest");
 		model.addAttribute("mensaje", mensaje);
 		model.addAttribute("saludo", saludo);
-		model.addAttribute("persona", persona);
+		//model.addAttribute("persona", persona);
+		model.addAttribute("personas", personas);
 		return "index";
 	}
 }
